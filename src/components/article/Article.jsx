90da335b063951f9d97a1739092de98ff0ac4e3d
mirko-console-dir <cdmirko.saponaro@gmail.com>
2023-07-21 12:38:18 +1000

    add containers and components, add dependency react-icons, completed frontend for navbar/toggleMenu/header/brand/cta responsive

---
+++ src/components/article/Article.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './article.css';
+
+const Article = ({ imgUrl, date, text }) => (
+  <div className="">
+  
+  </div>
+);
+
+export default Article;
---
+++ src/components/feature/Feature.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './feature.css';
+
+const Feature = ({ title, text }) => (
+  <div className="">
+    
+  </div>
+);
+
+export default Feature;
